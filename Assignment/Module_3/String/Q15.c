5fcfe984c95e4054912e6cb6ece8db684382689c
Drashti Patel <drashtij20@gmail.com>
2024-09-09 08:09:16 +0530

    done

--- Assignment/Module_3/String/Q15.c
+++ Assignment/Module_3/String/Q15.c
@@ -1,36 +1,42 @@
 // Write a program in C to find the largest and smallest words in a string.
 
-#include<stdio.h>
-#include<string.h>
-int  main(){
-    char str[100],sna[10][20];
-    int i,j=0,k=0,m=0,n=0,max,min;
-    printf("Enter any sentence:");
-    gets(str);
-    for(i=0;str[i]!='\0';i++){
-        if(str[i]==' '){
-            sna[k][j]='\0';
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char sentence[100];
+    char words[10][20];
+    int i = 0,k=0,j=0;
+    int min,max;
+    int m=0,n=0;
+    printf("Enter any Sentence:");
+    gets(sentence);
+   
+    for( i = 0; sentence[i] != '\0';i++){
+        if(sentence[i] != ' '){
+            words[k][j] = sentence[i];
+            j++;
+        }
+        else{
+            words[k][j] ='\0';
             k++;
             j=0;
         }
-        else{
-            sna[k][j]=str[i];
-            j++;
+    }
+    words[k][j] = '\0';
+    
+    min =strlen(words[0]);
+    max = strlen(words[0]);
+    
+    for(i = 0;i<=k;i++){
+        if(min>strlen(words[i])){
+            min = words[i];
+            m = i;
+        }
+        if(max<strlen(words[i])){
+            max = words[i];
+            n = i;
         }
     }
-    sna[k][j]='\0';
-    max=strlen(sna[0]);
-    min=strlen(sna[0]);
-    for(i=0;i<=k;i++)
-    {
-        if(max<strlen(sna[i]))
-        {
-            m=i;
-        }
-        if(min>strlen(sna[i]))
-        {
-            n=i;
-        }
-    }
-    printf("\nLargest word : %s \nsmallest word : %s",sna[m],sna[n]);
+    printf("The Largest Word In The Given Sentence is : %s",sentence[n]);
+    printf("The Smallest Word In The Given Sentence is : %s",sentence[m]);
 }
